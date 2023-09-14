cfb4bdcc941dda7267ead155193655a92ba2a526
beratberkayg <berat.1675@gmail.com>
2023-09-14 14:17:32 +0300

    update

--- components/src/App.jsx
+++ components/src/App.jsx
@@ -1,10 +1,15 @@
+import Text from "./components/text";
+
 function App() {
   let name = "berat berkay";
   let surname = "gökdemir";
   return (
-    <div>
-      {name} {surname}
-    </div>
+    <>
+      <div>react eğitimi1</div>
+      <div>react eğitimi2</div>
+      <div>react eğitimi3</div>
+      <Text />
+    </>
   );
 }
 
