10d2383ac3921e126b9491910e96890e06aa9ba3
beratberkayg <berat.1675@gmail.com>
2023-09-14 14:31:38 +0300

    create props

---
+++ props/src/App.jsx
@@ -0,0 +1,17 @@
+import Text from "./components/text";
+
+function App() {
+  //props ...
+  return (
+    <>
+      <div>react eğitimi1</div>
+      <div>react eğitimi2</div>
+      <div>react eğitimi3</div>
+      <Text />
+      <Text />
+      <Text />
+    </>
+  );
+}
+
+export default App;
