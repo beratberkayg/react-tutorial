20e4acf6ae421a42e4adbd42e21dca53733b1a39
beratberkayg <berat.1675@gmail.com>
2023-09-15 14:04:24 +0300

    update

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -2,11 +2,23 @@ import { useState } from "react";
 
 function App() {
   const [name, setName] = useState(null);
+  const [data, setData] = useState(null);
 
   console.log(name);
+
+  const targetFunc = (e) => {
+    setName(e.target.value);
+  };
+
+  const clickFunc = () => {
+    setData(name);
+  };
+
   return (
     <>
-      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <input type="text" onChange={targetFunc} />
+      <button onClick={clickFunc}>Tıkla</button>
+      <div> {data}</div>
     </>
   );
 }
