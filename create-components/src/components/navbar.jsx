d0a9ee86e6e4b6c6cef1acee6cac3f95a9265621
beratberkayg <berat.1675@gmail.com>
2023-09-18 20:20:07 +0300

    create navbar.jsx

---
+++ create-components/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>ANASAYFA</li>
+        <li>HAKKIMDA</li>
+        <li>ILETISIM</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
