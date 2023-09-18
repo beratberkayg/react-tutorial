056636a7201294654196cba12926fcac673eaf22
beratberkayg <berat.1675@gmail.com>
2023-09-18 20:26:38 +0300

    update

--- create-components/src/App.jsx
+++ create-components/src/App.jsx
@@ -5,11 +5,13 @@ import "./App.css";
 import Name from "./components/name";
 import Job from "./components/job";
 import Text from "./components/text";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <>
       <div className="container">
+        <Navbar />
         <Name />
         <Job />
         <Text />
