ac8404da7527c8253e8b71bc0d078adeb71edcef
beratberkayg <berat.1675@gmail.com>
2023-09-22 16:07:00 +0300

    update

--- react-app/src/pages/Detail.jsx
+++ react-app/src/pages/Detail.jsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom";
+
 const Detail = () => {
+  const { id } = useParams();
+
   return <div>Detail</div>;
 };
 
--- react-app/src/pages/Home.jsx
+++ react-app/src/pages/Home.jsx
@@ -1,5 +1,39 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  const data = [
+    {
+      id: 0,
+      name: "react",
+      description: "react açıklama",
+    },
+    {
+      id: 1,
+      name: "vue",
+      description: "vue açıklama",
+    },
+    {
+      id: 2,
+      name: "angular",
+      description: "angular açıklama",
+    },
+  ];
+  const navigate = useNavigate();
+  return (
+    <div>
+      {data.map((item) => (
+        <div
+          onClick={() => navigate(`detail/${item.id}`)}
+          style={{ cursor: "pointer", marginBottom: "30px" }}
+          key={item.id}
+        >
+          <h1>{item.name}</h1>
+          <p>{item.description}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
