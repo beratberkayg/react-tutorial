9b35fac22504eb8051a99dcd3e08b951541878f7
beratberkayg <berat.1675@gmail.com>
2023-09-22 16:47:15 +0300

    update

--- react-app/src/pages/Detail.jsx
+++ react-app/src/pages/Detail.jsx
@@ -1,8 +1,28 @@
 import { useParams } from "react-router-dom";
+import { data } from "./Home";
+import { useEffect, useState } from "react";
 
 const Detail = () => {
   const { id } = useParams();
-  return <div>Detail</div>;
+
+  const [getData, setGetData] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      setGetData(data.find((dt) => dt.id == id));
+    }
+  }, [id]);
+  console.log(getData);
+
+  return (
+    <div>
+      <div>
+        {getData?.name}
+        <br />
+        {getData?.description}
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
